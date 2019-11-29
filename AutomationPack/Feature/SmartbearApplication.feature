3a3e54b1eb07a30753afa0f4ef3dc6317dff9b5f
tsainfotech <tsainfotech2019@gmail.com>
2019-11-29 10:18:03 +0530

    firstcommit

---
+++ AutomationPack/Feature/SmartbearApplication.feature
@@ -0,0 +1,12 @@
+Feature: Automation for smartt bear application 
+
+Scenario: verify login is success 
+
+Given open the browser
+When naviage into the application
+When enter username 
+When enter password 
+When click login 
+Then verify login is sucess
+
+ 
---
+++ AutomationPack/src/stepDefenition/Implementstion.java
@@ -0,0 +1,48 @@
+package stepDefenition;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Implementstion {
+
+	
+
+@Given("^open the browser$")
+public void open_the_browser() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+  
+}
+
+@When("^naviage into the application$")
+public void naviage_into_the_application() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    
+}
+
+@When("^enter username$")
+public void enter_username() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+   
+}
+
+@When("^enter password$")
+public void enter_password() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+   
+}
+
+@When("^click login$")
+public void click_login() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+   
+}
+
+@Then("^verify login is sucess$")
+public void verify_login_is_sucess() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+
+}
+
+
+}
